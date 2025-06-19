bb4d8ee65d78c0e0118afb9493b7b5f01a43f471
shamiulriyad <shamiulriyad1418@gmail.com>
2025-06-19 23:04:03 +0800

    cpp

--- Problem Solve/tempCodeRunnerFile.cpp
+++ Problem Solve/tempCodeRunnerFile.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void solve(int tc)
+{
+    int n;
+    cin>>n;
+		if(n<5){
+			cout<<-1<<endl;
+			continue;
+		}
+		for(int i=1;i<=n;i+=2)if(i!=5) cout<<i<<" ";
+		cout<<5<<" "<<4<<" ";
+		for(int i=2;i<=n;i+=2)if(i!=4) cout<<i<<" ";
+		cout<<endl;
+   
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int tc, tt = 1;
+    cin >> tt;
+
+    for(tc = 1; tc <= tt; tc++)
+    {
+        solve(tc);
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ div 3/Round 988/C.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int tc)
+{
+    int n;
+    cin >> n;
+    if (n <= 4) {
+        cout << -1;
+    } else {
+        for (int i = 1; i <= n; i += 2) {
+            if (i == 5) continue;
+            cout << i << " ";
+        }
+        cout << "5 4 "; 
+        for (int i = 2; i <= n; i += 2) {
+            if (i == 4) continue;
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int tc, tt = 1;
+    cin >> tt;
+
+    for (tc = 1; tc <= tt; tc++) {
+        solve(tc);
+        cout << endl;
+    }
+
+    return 0;
+}
