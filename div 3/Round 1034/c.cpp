00ceaf24dc9b4eacfc1ac7fac85f0655f80200ad
shamiulriyad <shamiulriyad1418@gmail.com>
2025-07-03 23:38:16 +0800

    cpp

--- div 3/Round 1034/c.cpp
+++ div 3/Round 1034/c.cpp
@@ -1,41 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t; 
-    cin >> t;
-    while (t--) {
-        int n; 
-        cin >> n;
-        vector<int> a(n);
-        for (int &x : a) cin >> x;
-
-        vector<int> prefixMin(n), suffixMax(n);
-
-        // Compute prefix minimums
-        prefixMin[0] = a[0];
-        for (int i = 1; i < n; i++) {
-            prefixMin[i] = min(prefixMin[i-1], a[i]);
-        }
-
-        // Compute suffix maximums
-        suffixMax[n-1] = a[n-1];
-        for (int i = n-2; i >= 0; i--) {
-            suffixMax[i] = max(suffixMax[i+1], a[i]);
-        }
-
-        string result(n, '0');
-        for (int i = 0; i < n; i++) {
-            if (a[i] == prefixMin[i] && a[i] == suffixMax[i]) {
-                result[i] = '1';
-            }
-        }
-
-        cout << result << "\n";
-    }
-
+ 
+void solve(){
+    int n;cin >> n;
+    vector<int> a(n+1),pref(n+1,INT_MAX),suff(n+2);
+    for(int i=1;i<=n;i++) cin >> a[i];
+    
+    for(int i=1;i<=n;i++) pref[i] = min(pref[i-1],a[i]);
+ 
+    for(int i=n;i>=1;i--) suff[i] = max(suff[i+1],a[i]);
+ 
+    for(int i=1;i<=n;i++) cout << (a[i]==pref[i] || a[i]==suff[i]) ? 1 : 0;
+ 
+    cout << '\n';
+}
+ 
+int main(){
+    int t;cin >> t;
+    while(t--) solve();
     return 0;
 }
