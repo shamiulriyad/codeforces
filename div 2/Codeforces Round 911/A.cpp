9685ce1a9d702422157ea37bafc501044de73c2a
shamiulriyad <shamiulriyad1418@gmail.com>
2025-10-09 22:06:39 +0600

    Cover in Water

---
+++ div 2/Codeforces Round 911/A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(int tc)
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+ 
+    int count = 0, ans = 0;
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '.') {
+            count++;
+            ans++;
+            if(count >= 3) {  
+                cout << 2;
+                return;
+            }
+        } else {
+            count = 0; 
+        }
+    }
+    cout << ans;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int tt;
+    cin >> tt;
+ 
+    for(int tc = 1; tc <= tt; tc++) {
+        solve(tc);
+        cout << "\n";
+    }
+ 
+    return 0;
+}
