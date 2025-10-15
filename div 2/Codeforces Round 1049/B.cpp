81fc82c791afca1b2ae09822e72ba8a160f6d230
shamiulriyad <shamiulriyad1418@gmail.com>
2025-10-15 23:56:37 +0600

    cpp

---
+++ div 2/Codeforces Round 1049/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int tc)
+{
+     int x;
+	    cin>>x;
+	    cout<<2*x<<endl;
+    
+    
+   
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int tc, tt = 1;
+    cin >> tt;
+
+    for(tc = 1; tc <= tt; tc++)
+    {
+        solve(tc);
+        
+        cout << endl;
+    }
+
+    return 0;
+}
